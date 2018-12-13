a9b4536bf6329b7618312d4c043ab5905c7f3260
Danny43 <39870573+Danny43@users.noreply.github.com>
2018-12-12 21:47:15 -0300

    otros retoques

--- registrarse.php
+++ registrarse.php
@@ -75,7 +75,7 @@ session_start();
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Cocineros Unidos</a>
+                <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="img/logo.png">Cocineros Unidos</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
